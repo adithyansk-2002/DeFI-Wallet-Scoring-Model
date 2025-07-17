e5efea8495e4d99940bf5149d5e148f9d1abd014
Adithyan Suresh Kumar <160021587+adithyansk-2002@users.noreply.github.com>
2025-07-17 18:40:58 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+scores = pd.read_csv('wallet_scores.csv')
+bins = range(0, 1100, 100)
+scores['score_range'] = pd.cut(scores['credit_score'], bins=bins)
+
+# Count per bucket
+print(scores['score_range'].value_counts().sort_index())
+
+# Plot
+scores['credit_score'].hist(bins=20, edgecolor='black')
+plt.title('Credit Score Distribution')
+plt.xlabel('Score')
+plt.ylabel('Number of Wallets')
+plt.grid(True)
+plt.show()
